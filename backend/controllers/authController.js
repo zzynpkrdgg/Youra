6f7879150089c708eb5cff6324c64e74fc12255d
Zeynep Karadağ <zzynpkrdgg@gmail.com>
2026-05-15 21:49:45 +0300

    auth

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const User = require("../models/user");
+const User = require("../models/User");
 
 const testAuth = (req, res) => {
     res.json({
