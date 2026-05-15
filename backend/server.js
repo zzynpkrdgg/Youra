908591fcc60a48edb27576d4b570dd716d0e1094
baharturan <turanbahar131@gmail.com>
2026-05-15 17:38:33 +0300

    backend ayağa kaldırma ve dosya yapılarını düzenleme

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+require("dotenv").config()
+
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+
+// Middleware
+app.use(cors())
+app.use(express.json())
+
+// Test Route
+app.get("/", (req, res) => {
+    res.send("Youra API çalışıyor")
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
